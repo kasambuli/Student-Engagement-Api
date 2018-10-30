d4bf892bb2fe2d6107ecef0553a9554bdc0203b2
kasambuli <cynthiakasambuli@gmail.com>
2018-10-30 23:33:55 +0300

    added the app urls to the project urls

--- studentEngagement/urls.py
+++ studentEngagement/urls.py
@@ -13,10 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
+from django.conf.urls import url,include
 from django.contrib import admin
+from rest_framework.urlpatterns import format_suffix_patterns
+from engagementApp import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^/', include('engagementApp.urls')),
+    url(r'',include('engagementApp.urls')),
 ]
