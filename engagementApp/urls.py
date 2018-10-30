043cadfc66e6141f89c7e73192d2cd1caef87e5e
kasambuli <cynthiakasambuli@gmail.com>
2018-10-30 23:32:32 +0300

    added the necessary urls for the app

--- engagementApp/urls.py
+++ engagementApp/urls.py
@@ -1,1 +1,7 @@
 from django.conf.urls import url, include
+from . import views
+
+urlpatterns = [
+    url(r'^articles/', views.article_list),
+    url(r'^articles/<int:pk>/', views.single_article),
+]
