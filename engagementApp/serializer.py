85164897860fc56a9b5801cb865ec21433f86303
kasambuli <cynthiakasambuli@gmail.com>
2018-11-06 12:10:40 +0300

    added UserSerializer class to the file

--- engagementApp/serializer.py
+++ engagementApp/serializer.py
@@ -5,3 +5,9 @@ class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Articles
         fields='__all__'
+
+class UserSerializer(serializers.ModelSerializer):
+    articles = serializers.PrimaryKeyRelatedField(many=True, queryset=Articles.objects.all())
+    class Meta:
+        model=User
+        fields = ('id','username','articles')
