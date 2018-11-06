dd163fd356e01ef609285a63bf3cd1f1ea3f6711
kasambuli <cynthiakasambuli@gmail.com>
2018-11-06 12:12:06 +0300

    changed the url for viewing a single item in the Api

--- engagementApp/urls.py
+++ engagementApp/urls.py
@@ -1,7 +1,10 @@
-from django.conf.urls import url, include
+from django.conf.urls import url
+from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = [
-    url(r'^articles/', views.article_list),
-    url(r'^articles/<int:pk>/', views.single_article),
+
+    url(r'articles/', views.ArticleList.as_view()),
+    url(r'article/(?P<pk>\d+)/', views.ArticleDetail.as_view()),
 ]
+urlpatterns = format_suffix_patterns(urlpatterns)
