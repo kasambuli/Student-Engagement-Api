28486c4d4ede580b5662b3e67ff48b09646b5d2e
kasambuli <cynthiakasambuli@gmail.com>
2018-11-06 12:09:53 +0300

    added owner column and save method to the Articles class

--- engagementApp/models.py
+++ engagementApp/models.py
@@ -1,5 +1,8 @@
 from django.conf import settings
 from django.db import models
+from pygments.lexers import get_lexer_by_name
+from pygments.formatters.html import HtmlFormatter
+from pygments import highlight
 
 # Create your models here.
 class Articles(models.Model):
@@ -8,6 +11,19 @@ class Articles(models.Model):
     # image = models.ImageField()
     category = models.CharField(max_length=40)
     author = models.CharField(max_length=40)
+    owner = models.ForeignKey(
+        'auth.User', related_name='articles', on_delete=models.CASCADE, null=True)
+    highlighted = models.TextField(null = True)
+    def save(self, *args, **kwargs):
+        """
+        Use the `pygments` library to create a highlighted HTML
+        representation of the code snippet.
+        """
+        lexer= get_lexer_by_name(self.language)
+        linenos = 'table' if self.linenos else False
+        options = {'title':self.title} if self.title else {}
+        formatter = HtmlFormatter(style=self.style, linenos=linenos, full=True, **options)
+        self.highlighted = highlight(self.code, lexer, formatter)
+        super(Articles, self).save(*args, **kwargs)
 
-    def __str__(self):
-        return self.title 
+    
