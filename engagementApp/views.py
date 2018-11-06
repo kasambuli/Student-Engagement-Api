2205361d9efd38b2fcb6b89455d97ee918f1cfdf
kasambuli <cynthiakasambuli@gmail.com>
2018-11-06 12:13:23 +0300

    simplifiedthe views for viewing the APi

--- engagementApp/views.py
+++ engagementApp/views.py
@@ -1,56 +1,35 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.renderers import JSONRenderer
-from rest_framework.parsers import JSONParser
+# from django.http import HttpResponse, JsonResponse
+# from rest_framework.renderers import JSONRenderer
+# from rest_framework.parsers import JSONParser+
+# from django.http import Http404
+# from django.views.decorators.csrf import csrf_exempt
+# from rest_framework import status
+# from rest_framework.decorators import APIView
+# from rest_framework.response import Response
+# from rest_framework import mixins
+from rest_framework import generics
 from .models import Articles
 from .serializer import ArticleSerializer
 
-@csrf_exempt
-def article_list(request):
+
+ #this is the one for getting all the articles. So idealy, we get the objects from the model class then serialize them and return as Json data 
+
+class ArticleList(generics.ListCreateAPIView):
     """
-    Lists all the articles or create a new article
+    List all snippets, or create a new snippet.
     """
+    queryset = Articles.objects.all()
+    serializer_class = ArticleSerializer
+   
 
-    #this is the one for getting all the articles. So idealy, we get the objects from the model class then serialize them and return as Json data 
-    if request.method == 'GET':
-        articles = Articles.objects.all()
-        serializer = ArticleSerializer(articles, many=True)
-        return JsonResponse(serializer.data, safe=False)
-
-    #this is the one for creating a new article, so idealy we deserialize the json data inputed, then change it to python native  datatypes by parsing, then into objects by serializing the data
-    elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = ArticleSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
-
-@csrf_exempt
-def single_article(request, pk):
+class ArticleDetail(generics.RetrieveUpdateDestroyAPIView):
     """
-    retrieve, update or delete an article
+    Retrieve, update or delete an article instance.
     """
-    try:
-        article = Articles.objects.get(pk=pk)
-    except Articles.DoesNotExist:
-        return HttpResponse(status=404)
+    queryset = Articles.objects.all()
+    serializer_class = ArticleSerializer
 
-    if request.method == 'GET':
-        serializer = ArticleSerializer(article)
-        return JsonResponse(serializer.data)
-
-    elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = ArticleSerializer(article, data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
-
-    elif request.method == 'DELETE':
-        article.delete()
-        return HttpResponse(status=204)
+    
