f05899920bca89e9f2f3fa71a0dd72593e7867e4
kasambuli <cynthiakasambuli@gmail.com>
2018-11-12 22:35:41 +0300

    added the Userserializer class

--- engagementApp/serializer.py
+++ engagementApp/serializer.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from .models import Articles
-
+from django.contrib.auth.models import User
 class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Articles
