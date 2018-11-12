9f24172c9229bf2522aec1563672b952cdf4a794
kasambuli <cynthiakasambuli@gmail.com>
2018-11-12 22:35:56 +0300

    added the User urls

--- engagementApp/urls.py
+++ engagementApp/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
 
     url(r'articles/', views.ArticleList.as_view()),
     url(r'article/(?P<pk>\d+)/', views.ArticleDetail.as_view()),
+    url(r'users/', views.UserList.as_view()),
+    url(r'user/(?P<pk>\d+)/', views.UserDetail.as_view()),
 ]
 urlpatterns = format_suffix_patterns(urlpatterns)
