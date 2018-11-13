d9d2af47ae4977af1a69d4726f92cb830d22b2de
kasambuli <cynthiakasambuli@gmail.com>
2018-11-14 00:32:49 +0300

    added the url to logging in as a user

--- engagementApp/urls.py
+++ engagementApp/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
@@ -9,4 +9,6 @@ urlpatterns = [
     url(r'users/', views.UserList.as_view()),
     url(r'user/(?P<pk>\d+)/', views.UserDetail.as_view()),
 ]
-urlpatterns = format_suffix_patterns(urlpatterns)
+urlpatterns += [
+    url(r'api-auth/', include('rest_framework.urls')),
+]
