cfe30b1c5f4bfd68cb36a3b35ff8191f2f742283
kasambuli <cynthiakasambuli@gmail.com>
2018-11-14 00:30:39 +0300

    commented out the save method that included the highlighting and language for it to work now but I will go back to it

--- engagementApp/models.py
+++ engagementApp/models.py
@@ -14,16 +14,16 @@ class Articles(models.Model):
     owner = models.ForeignKey(
         'auth.User', related_name='articles', on_delete=models.CASCADE, null=True)
     highlighted = models.TextField(null = True)
-    def save(self, *args, **kwargs):
-        """
-        Use the `pygments` library to create a highlighted HTML
-        representation of the code snippet.
-        """
-        lexer= get_lexer_by_name(self.language)
-        linenos = 'table' if self.linenos else False
-        options = {'title':self.title} if self.title else {}
-        formatter = HtmlFormatter(style=self.style, linenos=linenos, full=True, **options)
-        self.highlighted = highlight(self.code, lexer, formatter)
-        super(Articles, self).save(*args, **kwargs)
+    # def save(self, *args, **kwargs):
+    #     """
+    #     Use the `pygments` library to create a highlighted HTML
+    #     representation of the code snippet.
+    #     """
+    #     lexer= get_lexer_by_name(self.language)
+    #     linenos = 'table' if self.linenos else False
+    #     options = {'title':self.title} if self.title else {}
+    #     formatter = HtmlFormatter(style=self.style, linenos=linenos, full=True, **options)
+    #     self.highlighted = highlight(self.code, lexer, formatter)
+    #     super(Articles, self).save(*args, **kwargs)
 
     
