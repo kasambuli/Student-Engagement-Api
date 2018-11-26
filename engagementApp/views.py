c2544a155be3eea7d1836ab3dbd54121b74543fb
kasambuli <cynthiakasambuli@gmail.com>
2018-11-26 11:42:43 +0300

    added the highlight view and the view to the root of the api

--- engagementApp/views.py
+++ engagementApp/views.py
@@ -7,8 +7,6 @@ from django.shortcuts import render
 # from django.http import Http404
 # from django.views.decorators.csrf import csrf_exempt
 # from rest_framework import status
-# from rest_framework.decorators import APIView
-# from rest_framework.response import Response
 # from rest_framework import mixins
 from rest_framework import generics
 from .models import Articles
@@ -16,7 +14,20 @@ from .serializer import ArticleSerializer, UserSerializer
 from django.contrib.auth.models import User
 from rest_framework import permissions
 from .permissions import IsOwnerOrReadOnly
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+from rest_framework import renderers
+
  #this is the one for getting all the articles. So idealy, we get the objects from the model class then serialize them and return as Json data 
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users':reverse('user-list', request=request, format=format),
+        'articles': reverse('article-list', request=request, format=format)
+    })
+
 
 class ArticleList(generics.ListCreateAPIView):
     """
@@ -43,3 +54,11 @@ class UserList(generics.ListAPIView):
 class UserDetail(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+class ArticleHighlight(generics.GenericAPIView):
+    queryset=Articles.objects.all()
+    render_classes=(renderers.StaticHTMLRenderer,)
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
