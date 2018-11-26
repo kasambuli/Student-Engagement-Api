fded91f3cced7d45a600beb949e1092a7c8d4b1c
kasambuli <cynthiakasambuli@gmail.com>
2018-11-26 11:39:30 +0300

    removed the save method from the Articles model

--- engagementApp/models.py
+++ engagementApp/models.py
@@ -14,15 +14,16 @@ class Articles(models.Model):
     owner = models.ForeignKey(
         'auth.User', related_name='articles', on_delete=models.CASCADE, null=True)
     highlighted = models.TextField(null = True)
+     
     # def save(self, *args, **kwargs):
     #     """
     #     Use the `pygments` library to create a highlighted HTML
     #     representation of the code snippet.
     #     """
-    #     lexer= get_lexer_by_name(self.language)
-    #     linenos = 'table' if self.linenos else False
+    #     # lexer= get_lexer_by_name(self.language)
+    #     # linenos = 'table' if self.linenos else False
     #     options = {'title':self.title} if self.title else {}
-    #     formatter = HtmlFormatter(style=self.style, linenos=linenos, full=True, **options)
+    #     formatter = HtmlFormatter(style=self.style, full=True, **options)
     #     self.highlighted = highlight(self.code, lexer, formatter)
     #     super(Articles, self).save(*args, **kwargs)
 
