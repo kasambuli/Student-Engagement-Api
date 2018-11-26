4aabbbaa0165f2f302a50b9ded5fee55923e4ca1
kasambuli <cynthiakasambuli@gmail.com>
2018-11-26 11:40:33 +0300

    added the urls to the url highlights

--- engagementApp/urls.py
+++ engagementApp/urls.py
@@ -4,10 +4,12 @@ from . import views
 
 urlpatterns = [
 
-    url(r'articles/', views.ArticleList.as_view()),
-    url(r'article/(?P<pk>\d+)/', views.ArticleDetail.as_view()),
-    url(r'users/', views.UserList.as_view()),
-    url(r'user/(?P<pk>\d+)/', views.UserDetail.as_view()),
+    url(r'articles/', views.ArticleList.as_view(), name = 'article-list'),
+    url(r'article/(?P<pk>\d+)/', views.ArticleDetail.as_view(), name = 'articles-detail'),
+    url(r'users/', views.UserList.as_view(), name='user-list'),
+    url(r'user/(?P<pk>\d+)/', views.UserDetail.as_view(), name='user-detail'),
+    # url(r'', views.api_root),
+    url(r'articles/(?P<pk>\d+)/highlight', views.ArticleHighlight.as_view(), name = 'article-highlight'),
 ]
 urlpatterns += [
     url(r'api-auth/', include('rest_framework.urls')),
