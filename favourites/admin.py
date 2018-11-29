833f5334a1e440e79688089b1e950d8d525926b2
kasambuli <cynthiakasambuli@gmail.com>
2018-11-29 19:27:59 +0300

    added comments model to the admin

--- favourites/admin.py
+++ favourites/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Favourite
+from .models import Favourite, Comments
 
 # Register your models here.
-admin.site.register(Favourite)
+admin.site.register(Favourite),
+admin.site.register(Comments)
