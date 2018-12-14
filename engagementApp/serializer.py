83b97408e9c13caec4a471087c86fba818ab2ccf
xv <xavierkibet@gmail.com>
2018-12-14 13:11:56 +0300

    yield favourites count instead of objects

--- engagementApp/serializer.py
+++ engagementApp/serializer.py
@@ -1,17 +1,22 @@
 from rest_framework import serializers
 from .models import Articles
 from django.contrib.auth.models import User
-from . import views 
+from . import views
 
 class ArticleSerializer(serializers.HyperlinkedModelSerializer):
-    owner = serializers.ReadOnlyField(source='owner.username'),
+    owner = serializers.ReadOnlyField(source='owner.username')
+    favourites = serializers.SerializerMethodField()
     # highlight = serializers.HyperlinkedIdentityField(
     #     view_name='users', format='html')
+
+    def get_favourites(self, obj):
+        return obj.favourites.count()
+
     class Meta:
         model = Articles
         fields = ('url', 'id', 'owner',
                   'title', 'content','image','category','author', 'favourites','comments')
-        
+
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     # articles = serializers.HyperlinkedRelatedField(
